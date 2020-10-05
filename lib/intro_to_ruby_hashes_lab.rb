2e1575621f9a1d924d2b0c1d544184d003c93cb5
Christopher Henderson <chenderson5280@gmail.com>
2020-10-05 22:32:35 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -11,9 +11,13 @@ end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  new_hash = {}
+  new_hash[:name] = "Grace Hopper"
 end
 
 
 def id_hash_generator(number)
   # return a hash with a key :id assigned to the provided number
+  new_hash = {}
+new_hash[:id] = number
 end
